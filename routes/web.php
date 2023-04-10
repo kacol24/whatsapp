f02f893e60680e6da62bd9116e266f0d2005ac08
Kevin Chandra <kacol.bot@gmail.com>
2023-04-10 17:41:47 +0700

    proof of concept whatsapp link

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::post('/', function (Request $request) {
+    $whatsappMessage = view('wa', [
+        'qty'    => $request->qty,
+        'title'  => 'Premium T-shirt',
+        'option' => [
+            [
+                'key'   => 'Color',
+                'value' => $request->option_1,
+            ],
+            [
+                'key'   => 'Size',
+                'value' => $request->option_2,
+            ],
+        ],
+        'url'    => route('home', ['option_1' => $request->option_1, 'option_2' => $request->option_2]),
+    ])->render();
+
+    return redirect()->away('https://wa.me/?text='.urlencode($whatsappMessage));
 });
